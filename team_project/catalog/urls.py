a0fa2777fdcc6fd06e970db3bf850f3340d2f1c6
0aaa <kova_op32@student.itstep.org>
2022-06-27 17:30:07 +0200

    PRELIMINARY views, urls, templates have been created.

--- team_project/catalog/urls.py
+++ team_project/catalog/urls.py
@@ -4,4 +4,29 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('user/<int:pk>', views.UserDetailView.as_view(), name='user'),
+    path('transfer/', views.TransferView, name='transfer'),
+    path('my-income/', views.IncomeView.as_view(), name='my-income'),
+    path('my-expenses/', views.ExpensesView.as_view(), name='my-expenses'),
+
+    path('users/', views.UserListView.as_view(), name='users'),
+    path('currencies/', views.CurrenciesListView.as_view(), name='currencies'),
+    path('income/', views.IncomeListView.as_view(), name='income'),
+    path('expenses/', views.ExpensesListView.as_view(), name='expenses'),
+
+    path('user/create/', views.UserCreate.as_view(), name='user-create'),
+    path('user/<int:pk>/update/', views.UserUpdate.as_view(), name='user-update'),
+    path('user/<int:pk>/delete/', views.UserDelete.as_view(), name='user-delete'),
+
+    path('currency/create/', views.CurrencyCreate.as_view(), name='currency-create'),
+    path('currency/<int:pk>/update/', views.CurrencyUpdate.as_view(), name='currency-update'),
+    path('currency/<int:pk>/delete/', views.CurrencyDelete.as_view(), name='currency-delete'),
+
+    path('income/create/', views.IncomeCreate.as_view(), name='income-create'),
+    path('income/<int:pk>/update/', views.IncomeUpdate.as_view(), name='income-update'),
+    path('income/<int:pk>/delete/', views.IncomeDelete.as_view(), name='income-delete'),
+
+    path('expense/create/', views.ExpenseCreate.as_view(), name='expense-create'),
+    path('expense/<int:pk>/update/', views.ExpenseUpdate.as_view(), name='expense-update'),
+    path('expense/<int:pk>/delete/', views.ExpenseDelete.as_view(), name='expense-delete'),
 ]
--- team_project/catalog/views.py
+++ team_project/catalog/views.py
@@ -1,5 +1,139 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+# from .models import Currency, Expense, Income, Transfer
 
 
 def index(request):
     return render(request, 'catalog/index.html')
+
+
+class UserDetailView(generic.DetailView):
+    model = User
+
+
+class TransferView(LoginRequiredMixin):
+    # model = Transfer
+    model = User
+
+
+class IncomeView(LoginRequiredMixin, generic.ListView):# Income list for a specific user.
+    # model = Income
+    model = User
+
+
+class ExpensesView(LoginRequiredMixin, generic.ListView):# Expenses list for a specific user.
+    # model = Expense
+    model = User
+
+
+
+class UserListView(PermissionRequiredMixin, generic.ListView):
+    permission_required = 'catalog.admin_required'
+    model = User
+    paginate_by = 10
+
+
+class CurrenciesListView(PermissionRequiredMixin, generic.ListView):
+    permission_required = 'catalog.admin_required'
+    # model = Currency
+    model = User
+    paginate_by = 10
+
+
+class IncomeListView(PermissionRequiredMixin, generic.ListView):
+    permission_required = 'catalog.admin_required'
+    # model = Income
+    model = User
+    paginate_by = 10
+
+
+class ExpensesListView(PermissionRequiredMixin, generic.ListView):
+    permission_required = 'catalog.admin_required'
+    # model = Expense
+    model = User
+    paginate_by = 10
+
+
+
+class UserCreate(PermissionRequiredMixin, CreateView):
+    permission_required = 'catalog.admin_required'
+    model = User
+
+
+class UserUpdate(PermissionRequiredMixin, UpdateView):
+    permission_required = 'catalog.admin_required'
+    model = User
+    success_url = reverse_lazy('users')
+
+
+class UserDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = 'catalog.admin_required'
+    model = User
+    success_url = reverse_lazy('users')
+
+
+
+class CurrencyCreate(PermissionRequiredMixin, CreateView):
+    permission_required = 'catalog.admin_required'
+    # model = Currency
+    model = User
+
+
+class CurrencyUpdate(PermissionRequiredMixin, UpdateView):
+    permission_required = 'catalog.admin_required'
+    # model = Currency
+    model = User
+    success_url = reverse_lazy('currencies')
+
+
+class CurrencyDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = 'catalog.admin_required'
+    # model = Currency
+    model = User
+    success_url = reverse_lazy('currencies')
+
+
+
+class IncomeCreate(PermissionRequiredMixin, CreateView):
+    permission_required = 'catalog.admin_required'
+    # model = Income
+    model = User
+
+
+class IncomeUpdate(PermissionRequiredMixin, UpdateView):
+    permission_required = 'catalog.admin_required'
+    # model = Income
+    model = User
+    success_url = reverse_lazy('income')
+
+
+class IncomeDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = 'catalog.admin_required'
+    # model = Income
+    model = User
+    success_url = reverse_lazy('income')
+
+
+
+class ExpenseCreate(PermissionRequiredMixin, CreateView):
+    permission_required = 'catalog.admin_required'
+    # model = Expense
+    model = User
+
+
+class ExpenseUpdate(PermissionRequiredMixin, UpdateView):
+    permission_required = 'catalog.admin_required'
+    # model = Expense
+    model = User
+    success_url = reverse_lazy('expenses')
+
+
+class ExpenseDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = 'catalog.admin_required'
+    # model = Expense
+    model = User
+    success_url = reverse_lazy('expenses')
