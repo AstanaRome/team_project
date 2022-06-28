7c0fff7d1370aafffc8b7718d124e65b40d40c7d
ilyas <a@mail.ru>
2022-06-28 19:36:39 +0600

    добавил в админку модели

--- team_project/catalog/admin.py
+++ team_project/catalog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(ExchangeRate)
+admin.site.register(Currency)
+admin.site.register(ExpenseCategory)
+admin.site.register(IncomeCategory)
+admin.site.register(Wallet)
+admin.site.register(Expenditure)
+admin.site.register(Transfer)
+admin.site.register(Income)
